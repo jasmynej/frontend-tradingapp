193bd9b578a8cfbd53ec33595f2996f774e7ef70
Jasmyne Jean-Remy <jasmynej14@gmail.com>
2022-04-05 16:38:23 -0400

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/screens/*.{js,jsx,ts,tsx}",
+    "./src/components/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
